48a4f8d4e0d27f2d35b1a992e4224401fbe648cf
oukalatte <oukalatte@gmail.com>
2023-04-12 08:46:57 +0800

    i can do better

--- utils/useLenis.ts
+++ utils/useLenis.ts
@@ -20,7 +20,7 @@ const useScroll = () => {
     const lenis = new Lenis({
       duration: 1,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothTouch: true,
+      // smoothTouch: true,
     });
 
     setLenis(lenis);
